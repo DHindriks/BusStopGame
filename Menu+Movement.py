1b73916e3c63a398bfd1a2683f275d153a9b9ccc
RealCaptainObvious <58974439+RealCaptainObvious@users.noreply.github.com>
2024-04-13 19:12:17 +0200

    Mneu button + Detection, no countdown yet
    see title

--- Menu+Movement.py
+++ Menu+Movement.py
@@ -52,8 +52,11 @@ box_y = screen_height - box_height
 left_box_x = 50 
 right_box_x = screen_width - box_width - 50
 
-
+#checks if the menu needs to pop-up
 menuTime = True
+
+#is the mouse inside a button?
+mouseOnButton = False
 
 #Run!
 run = True
@@ -73,36 +76,55 @@ class menuButton(pygame.sprite.Sprite):
 		self.buttonWidth = buttonWidth
 		self.buttonHeight = buttonHeight
 		self.buttonText = buttonText
+		
 		self.color = (70,70,200)
+		self.rect = pygame.Rect(self.buttonX, self.buttonY, self.buttonWidth, self.buttonHeight)
+		
 		self.textColor = (180, 240, 255)
 		self.font = pygame.font.SysFont('Arial', 25)
-	
-		
-	def mouseInside():
-		#Code to check if mouse is inside
-		#Code to change the button color
-		menuTime = False
+
+		self.transition_duration = 30
+		self.transition_timer = 0
+		self.target_color = (70,70,200)
 		
 	def draw(self):
-		pygame.draw.rect(self.canvas, self.color, (self.buttonX, self.buttonY, self.buttonWidth, self.buttonHeight))
-		self.canvas.blit(self.font.render(self.buttonText, True, self.textColor), (self.buttonX, self.buttonY))
+		pygame.draw.rect(self.canvas, self.color, self.rect)
+		self.draw_centered_text()
+	
+	def draw_centered_text(self):
+		self.text_surface = self.font.render(self.buttonText, True, self.textColor)
+		self.text_rect = self.text_surface.get_rect()
+
+		rect_center = self.rect.center
+
+		self.text_x = rect_center[0] - self.text_rect.width / 2
+		self.text_y = rect_center[1] - self.text_rect.height / 2
+
+		self.text_rect.topleft = (self.text_x, self.text_y)
+
+		self.canvas.blit(self.text_surface, self.text_rect)
 		
+
+
+class mouse(pygame.sprite.Sprite):
+	def __init__(self, width, height, screen):
+		super().__init__()
+		self.canvas = screen
+		self.hbWidth = width
+		self.hbHeight = height
 		
+		self.rect = pygame.Rect(0, 0, self.hbWidth, self.hbHeight)
+	
+	def update(self):
+		self.rect.topleft = pygame.mouse.get_pos()
 	
 startButton = menuButton(screen_width*0.1, (screen_height/2)-50, 200, 100, "Start", win)
 endButton = menuButton((screen_width*0.9)-200, (screen_height/2)-50, 200, 100, "End", win)
+theMouse = mouse(1, 1, win)
 
-6
 #Runs the code as long as the game is running
-while run: 
-	pygame.time.delay(10)
+while run:
 	
-	balloon_col = pygame.Rect(balx, baly, 37, 49)
-	plat1_col = pygame.Rect(plat1x, plat1y, width, height)
-	plat2_col = pygame.Rect(plat2x, plat2y, width, height)
-	basketL_col = pygame.Rect(left_box_x, box_y + box_height * 0.9, box_width, box_height)
-	basketR_col = pygame.Rect(right_box_x, box_y + box_height * 0.9, box_width, box_height)
-
 	for event in pygame.event.get(): 
 		if event.type == pygame.QUIT: 
 			run = False
@@ -110,14 +132,27 @@ while run:
 	#All the Main Menu Stuff happens here
 	if menuTime:
 		win.fill((32, 32, 32))
-
-		#Draw Menu Button
-		startButton.draw()
-		endButton.draw()
 		
+		#Draw a hitbox around the mouse
+		theMouse.update()
+			
+		# Check for collision
+		if startButton.rect.colliderect(theMouse.rect):
+			print ("hello")
+		
+		# Draw button
+		startButton.draw()
+		
+		endButton.draw()
 	
 	#If the player chooses start then:
 	else:
+		balloon_col = pygame.Rect(balx, baly, 37, 49)
+		plat1_col = pygame.Rect(plat1x, plat1y, width, height)
+		plat2_col = pygame.Rect(plat2x, plat2y, width, height)
+		basketL_col = pygame.Rect(left_box_x, box_y + box_height * 0.9, box_width, box_height)
+		basketR_col = pygame.Rect(right_box_x, box_y + box_height * 0.9, box_width, box_height)
+		
 		keys = pygame.key.get_pressed() 
 
 		#Movement for left platform
